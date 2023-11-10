98f315d534a3b210760c65b214f564c5d10d388e
antmrlt <antoine.mourlot@proton.me>
2023-11-10 10:35:10 +0100

    update php

--- Contact/form.php
+++ Contact/form.php
@@ -1,8 +1,26 @@
 <?php
-    $to = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $headers = "From: antoine.mourlot@gmail.com\r\n";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérer les données du formulaire
+    $nom = $_POST["name"];
+    $nom_de_famille = $_POST["last-name"];
+    $email = $_POST["mail"];
+    $telephone = $_POST["phone"];
+    $message = $_POST["message"];
 
-    mail($to, $subject, $message, $headers);
+    // Adresse e-mail de destination
+    $destinataire = "votre@email.com";
+
+    // Sujet du message
+    $sujet = "Nouveau message de la part de $nom";
+
+    // Corps du message
+    $corps_message = "Nom: $nom\n";
+    $corps_message .= "Nom de Famille: $nom_de_famille\n";
+    $corps_message .= "E-mail: $email\n";
+    $corps_message .= "Téléphone: $telephone\n\n";
+    $corps_message .= "Message:\n$message";
+
+    // En-têtes du message
+    $headers = "De: $email";
+}
 ?>
